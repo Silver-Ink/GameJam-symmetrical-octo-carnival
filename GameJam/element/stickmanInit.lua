43aca204a17882dd230a19ef6d20a1dd557eadd3
Martin <martin.74@orange.fr>
2022-03-12 22:35:30 +0100

    animation déplacements

--- GameJam/element/stickmanInit.lua
+++ GameJam/element/stickmanInit.lua
@@ -1,9 +1,25 @@
+local function Draw(elem)
+  --love.graphics.rectangle("fill", elem.hitbox.x, elem.hitbox.y, 1, 2)
+  local img = elem.sprite.image
+
+  local ap = 0.1
+  local speed = 5
+  local time = (TIME * speed)%6.28318
+
+  local width, height = img:getDimensions( )
+  print(math.abs(math.sin(time) * ap * 2))
+  love.graphics.draw(img, elem.hitbox.x + (width/2) / 256, elem.hitbox.y + (height/2)/ 256 - math.abs(math.cos(time) * ap * 2),
+   math.sin(time) * ap, 1/width, 1/width,
+   width/2, height/2,
+     0, math.sin(time) * ap)
+end
+--Init player
 return function (elem, arg)
   local rect = require("../rectangle")
   local sprite = require("../sprite")
 
-  elem.draw = require("element.elementDraw")
+  elem.draw = Draw
   elem.update = require("element.stickmanUpdate")
   elem.hitbox = rect.create(arg.x or 0, arg.y or 0, 1, 1)
-  elem.sprite = sprite.create("Content/stickman.png")
+  elem.sprite = sprite.create("Content/as.png")
 end
