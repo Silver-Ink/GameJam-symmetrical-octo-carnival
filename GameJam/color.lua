a9e8438337f0f14975e1763f9bad32cc1be2e8a7
Thomas Mewily <40406187+Thomas36@users.noreply.github.com>
2022-03-12 16:42:03 +0100

    boutton budget++

--- GameJam/color.lua
+++ GameJam/color.lua
@@ -9,7 +9,15 @@ color.create = function(r, g, b, a)
   return { r=r, g=g, b=b, a=a}
 end
 
+color.lerp = function(oldColor, newColor, friction)
+  friction = friction or 0.9
+  local rfriction = 1-friction
+  return color.create(oldColor.r*friction + newColor.r*rfriction, oldColor.g*friction + newColor.g*rfriction, oldColor.b*friction + newColor.b*rfriction, oldColor.a*friction + newColor.a*rfriction)
+end
+
 color.white = color.create(1)
+color.black = color.create(0)
+color.gray = color.create(0.5)
 
 function color:apply()
   love.graphics.setColor(self.r,self.g,self.b,self.a)
