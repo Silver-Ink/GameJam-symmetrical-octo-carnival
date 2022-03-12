ab2efb50f4062e16fce6a4107a947a793024c50c
Thomas Mewily <40406187+Thomas36@users.noreply.github.com>
2022-03-12 16:17:40 +0100

    reseau boutton b1 b2

---
+++ GameJam/conf.lua
@@ -0,0 +1,4 @@
+
+
+io.stdout:setvbuf("no")
+if arg[#arg] == "-debug" then require("mobdebug").start() end -- debug pas à pas
--- GameJam/reseau.lua
+++ GameJam/reseau.lua
@@ -5,15 +5,25 @@ local rect = require("rectangle")
 local color = require("color")
 
 reseau.load = function()
-  reseau.b1 = button.create(rect.create(64,64,128,128))
+  reseau.b1 = button.create(rect.create(64,64,128,128), color.create(1, 0, 0))
+  reseau.b2 = button.create(rect.create(64+256*1,64,128,128), color.create(0, 1, 0))
+  print("reseaux")
+
 end
 
 reseau.update = function(dt)
+  if button.isPress(reseau.b1) then
+  print("boutton b1")
+  end
 
+  if button.isPress(reseau.b2) then
+    print("boutton b2")
+  end
 end
 
 reseau.draw = function()
   button.draw(reseau.b1)
+  button.draw(reseau.b2)
   --reseau.b1.draw()
 end
 
