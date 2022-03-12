94dbe7559d247e63768e0fc7455f0203dd436b17
Martin <martin.74@orange.fr>
2022-03-12 15:57:27 +0100

    Canvas working

--- GameJam/carte.lua
+++ GameJam/carte.lua
@@ -6,4 +6,19 @@ carte.Create = function ()
   return newCarte
 end
 
-return carte 
+function carte:Stroke(x1, y1, x2, y2)
+
+  love.graphics.setCanvas(self.image)
+
+  love.graphics.clear(1, 1, 1, 0.4)
+  love.graphics.line(x1, y1, x2, y2)
+
+  love.graphics.setCanvas()
+  
+end
+
+function carte:Draw()
+  love.graphics.draw(self.image, 150, 150, time / 10, 0.5, 0.5)
+end
+
+return carte
