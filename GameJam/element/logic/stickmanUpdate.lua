634df1d7398668970d6fab9236595def36717fde
Thomas Mewily <40406187+Thomas36@users.noreply.github.com>
2022-03-13 12:22:58 +0100

    player can move

--- GameJam/element/logic/stickmanUpdate.lua
+++ GameJam/element/logic/stickmanUpdate.lua
@@ -5,7 +5,7 @@ return function (elem)
   --(elem.mid or -1)
   if(elem.mid == MID) then
     local xAdd, yAdd = 0, 0
-    local speed =  1/16
+    local max_speed =  1/16
     if love.keyboard.isDown("right") then
       xAdd = 1
       --elem.hitbox.x = elem.hitbox.x + speed
@@ -15,11 +15,14 @@ return function (elem)
     end
   
     if love.keyboard.isDown("up") then
-      yAdd = 1
-    end
-    if love.keyboard.isDown("down") then
       yAdd = -1
     end
+    if love.keyboard.isDown("down") then
+      yAdd = 1
+    end
     
+    elem.hitbox.x = elem.hitbox.x + xAdd*max_speed
+    elem.hitbox.y = elem.hitbox.y + yAdd*max_speed
+
   end
 end
