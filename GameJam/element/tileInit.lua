99c21ef7125bcdfc9edd237107d1a936e6105596
Thomas Mewily <40406187+Thomas36@users.noreply.github.com>
2022-03-13 14:27:45 +0100

    tileinit

--- GameJam/element/tileInit.lua
+++ GameJam/element/tileInit.lua
@@ -3,6 +3,6 @@ return function (elem, arg)
   local sprite = require("../sprite")
 
   elem.draw = require("element.logic.tileDraw")
-  elem.hitbox = rect.create(arg.x or 1, arg.y or 1, 1, 1)
+  elem.hitbox = rect.create(arg.x or 1, arg.y or 1, arg.hx or 1, arg.hy or 1)
   elem.sprite = sprite.create("Content/"..(arg.name))
 end
