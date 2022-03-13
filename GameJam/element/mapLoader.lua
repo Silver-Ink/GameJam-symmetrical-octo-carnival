98f91497b1b0e973c4c77081aeb067d4d0166201
Thomas Mewily <40406187+Thomas36@users.noreply.github.com>
2022-03-13 15:19:25 +0100

    coffre

--- GameJam/element/mapLoader.lua
+++ GameJam/element/mapLoader.lua
@@ -10,23 +10,24 @@ mazeGenerator.Generate()
 local tileInit = require("element.tileInit")
 
 local function placeSolidBlock(x, y, texture)
-  local textures = {"wall.png","epouventail.png","tree_1.png"}
+  local textures = {"wall.png","epouventail.png","tree_1.png","plot.png"}
 
   local r = math.random()
   local alt = ""
-  if r < 0.9 then
+  if r < 0.85 then
     alt = textures[1]
-  elseif r < 0.95 then
+  elseif r < 0.9 then
     alt = textures[3]
-  else
+  elseif r < 0.95 then
     alt = textures[2]
+  else
+    alt = textures[4]
   end
   Game.create(tileInit, {x=2*x , y=2*y , name=texture or alt, hx=2,hy=2 }, nil, nil)
 end
 
 local function placeNonSolidBlock(x, y, texture)
-  
-  Game.create(tileInit, {x=2*x , y=2*y , name=texture or "chest_red_copen.png", hx=2,hy=2,isSolid = false}, nil, nil)
+  Game.create(tileInit, {x=2*x , y=2*y , name=texture or "chest_red_close.png", hx=2,hy=2,isSolid = false}, require("element.logic.chestUpdate"), nil)
 end
 
 
@@ -39,7 +40,7 @@ mapLoader.load = function (_)
         placeSolidBlock(column - mazeGenerator.numberOfColumns/2,row - mazeGenerator.numberOfRows/2) 
       end
       if mazeGenerator.grid[row][column] == "chest1" or mazeGenerator.grid[row][column] == "chest2" then
-        placeNonSolidBlock(column - mazeGenerator.numberOfColumns/2,row - mazeGenerator.numberOfRows/2,"chest_red_copen.png")
+        placeNonSolidBlock(column - mazeGenerator.numberOfColumns/2,row - mazeGenerator.numberOfRows/2,"chest_red_close.png")
       end
     end
 
